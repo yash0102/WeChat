0a58fba91244431dab7fb1e5b1433a0994f777d2
Yashwant Sonkar <yashsonkar0102@gmail.com>
2023-05-17 01:24:35 +0530

    Setting Up Front-End Part

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const express         = require('express');
 const app             = express();
 const port            = 8000;
+const path            = require('path');
 
-app.get('/' , (req, res) => {
-    res.send('<h1>Hello World </h1>')
-});
+// app.get('/' , (req, res) => {
+//     res.send('<h1>Hello World </h1>')
+// });
+
+
+// to serve static files stored in the ./assets directory 
+app.use(express.static(path.join(__dirname+'/assets')));
 
 app.listen(port , (err) => {
     if(err) {
